1b80128fa3f4f43550edd5b1a1372aad9bd1aca6
Macarena Moya <maca.moya@hotmail.com>
2020-03-25 00:10:51 -0300

    first commit

---
+++ src/ar/edu/utn/frsf/died/prueba/PruebaString.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.frsf.died.prueba;
+
+public class PruebaString {
+
+	public static void main(String[] args) {
+		
+		String s1 = "abc";
+		String s2 = "abc";
+		
+		System.out.println("s1 == s2: " + (s1==s2));
+		
+	}
+	
+}
